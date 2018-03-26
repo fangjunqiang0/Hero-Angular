0ca0443402f6935f79c23e8d7c24f4d19638f952
fjq <530743460@qq.com>
2018-03-26 16:38:41 +0800

    英雄列表

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 0, name: 'zhangqiushuang'},
+    {id: 1, name: 'fangjunqiang'},
+    {id: 2, name: 'liusong'},
+    {id: 3, name: 'chenjunheng'},
+    {id: 4, name: 'shixin'},
+    {id: 5, name: 'jiangxin'},
+    {id: 6, name: 'sunli'},
+    {id: 7, name: 'qiuzhiyong'},
+    {id: 8, name: 'tangdan'},
+    {id: 9, name: 'yeyusen'}
+];
+
