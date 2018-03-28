7d523f9cc28ad1a41b74e2f0d4b8e68f3694de2a
fjq <530743460@qq.com>
2018-03-28 17:24:49 +0800

    创建英雄详情

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    LoginComponent,
+    HomeComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
