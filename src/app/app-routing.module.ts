7e7b3b2a554a798c4e585bca566e0186bce93ea4
fjq <530743460@qq.com>
2018-03-29 16:25:10 +0800

    删除 home login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
-const routers: Routes = [
+export const routers: Routes = [
   {path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
+  exports: [RouterModule],
   // 这个方法之所以叫 forRoot()，是因为你要在应用的顶级配置这个路由器。
   // forRoot() 方法会提供路由所需的服务提供商和指令，还会基于浏览器的当前 URL 执行首次导航。
-  imports: [RouterModule.forRoot(routers)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routers)]
 })
 export class AppRoutingModule { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
@@ -17,8 +15,6 @@ import { AppRoutingModule } from './/app-routing.module';
   declarations: [
     AppComponent,
     HeroesComponent,
-    LoginComponent,
-    HomeComponent,
     HeroDetailComponent,
     MessagesComponent
   ],
